fcada23c9304c62886c6d5b61bd9258ee98ff16c
oubaidhl <hlaimiabid@gmail.com>
2021-08-13 11:12:39 +0100

    ghpage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   API_KEY: 'API_KEY=adsffsdfds6b-6727-46f4-8bee-2c6ce6293e41',
   prefUrlImage: 'http://localhost/back-end/images/products/',
   ID_CLIENT_PAYPAL: 'ASpvnQx3_QZBNsEX3MqxKlIy5RVtGMZJ2ezcFF8cSdc0pU76DrcxWXSdundURP8i3b0mODtIxBK8gGZV',
-  CURRENCY: 'USD'
+  CURRENCY: 'USD',
+  api_imageUser: 'http://localhost/back-end/images/users/'
 
 };
